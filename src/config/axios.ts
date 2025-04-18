fd9438aa4ea131bd5c241238fe82e94da75cd2d0
Bennett Smrdel <bennett@midwestdiesel.com>
2025-04-18 12:58:58 -0500

    fix: Get axios baseUrl

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,16 +1,15 @@
 import axios from 'axios';
 
+
 const getUrl = () => {
-  if (process.env.RAILWAY_ENVIRONMENT_NAME === 'production') {
+  if (import.meta.env.VITE_ENV_NAME === 'production') {
     return 'https://task-list-dev-challenge-server.up.railway.app';
   } else {
     return 'http://127.0.0.1:8080';
   }
 };
 
-
 const baseURL = getUrl();
-
 const api = axios.create({
   baseURL,
 });
