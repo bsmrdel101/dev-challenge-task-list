d867477b242b918448263009b8eff002f64319f9
Bennett Smrdel <bennett@midwestdiesel.com>
2025-04-18 10:03:52 -0500

    test: Create home page test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="header">
       <div className="header__title">
         <img src="/images/logo.svg" alt="Logo" draggable={false} />
-        <h1>My Task Board</h1>
+        <h1 data-testid="title">My Task Board</h1>
         <img src="/images/edit.svg" alt="Edit" draggable={false} />
       </div>
 
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 
-test('Has title', async ({ page }) => {
-  await page.goto('http://localhost:5173');
-  await expect(page).toHaveTitle('My Task Board');
+test.describe('Home', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173');
+  });
+
+  test('Load page', async ({ page }) => {
+    const title = page.getByTestId('title');
+    await expect(title).toBeVisible();
+  });  
 });
