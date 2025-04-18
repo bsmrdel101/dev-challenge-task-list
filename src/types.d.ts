1ffd16677f0a037199e52854928f9f7d488c61d3
Bennett Smrdel <bennett@midwestdiesel.com>
2025-04-18 10:15:57 -0500

    chore: Create type definintions

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,11 @@
 declare module '*.scss';
 
+type TaskStatus = 'In Progress' | 'Completed' | 'Won\'t Do';
+
 type Task = {
   id: number
   title: string
+  desc: string | null
+  icon: number
+  status: TaskStatus
 };
