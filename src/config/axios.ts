dae53152d3632f6a5ca77a4011c3fffbc75f61c1
Bennett Smrdel <bennett@midwestdiesel.com>
2025-04-18 12:48:14 -0500

    fix: Rename to RAILWAY_ENVIRONMENT_NAME

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getUrl = () => {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.RAILWAY_ENVIRONMENT_NAME === 'production') {
     return 'https://task-list-dev-challenge-server.up.railway.app';
   } else {
     return 'http://127.0.0.1:8080';
